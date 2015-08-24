a18a1f8834965fa28feeef87c8eb0106be56c5f0
fukuda <slave125xx@gmail.com>
2015-08-24 11:23:55 +0900

    適当設定

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,2 +1,9 @@
-server '192.168.117.158', user: 'vagrant', roles: %w{web app}
+set :application, 'vagrant_sample'
+set :repo_url, 'git@github.com:kobato-recipes/kobato-app.git'
 
+set :branch, 'master'
+set :deploy_to, '/var/www/html'
+
+server '192.168.33.10', user: 'vagrant', roles: %w{web app}
+
+set :linked_dirs, %w(logs )
